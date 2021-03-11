ad6051b9fa89bf2f1093addd581bc336600820d1
javapagar <apariciogarcia.j@gmail.com>
2021-03-11 20:08:26 +0100

    restructured project

--- src/app/dataUtil.py
+++ src/app/dataUtil.py
@@ -4,10 +4,10 @@ import streamlit as st
 #cargar datos
 @st.cache(suppress_st_warning=True)
 def cargarDelito():
-    return pd.read_csv('./data/clean/delitos.csv')
+    return pd.read_csv('./app/data/delitos.csv')
 @st.cache(suppress_st_warning=True)
 def cargarPoblacion():
-    return pd.read_csv ('./data/clean/poblacion.csv')
+    return pd.read_csv ('./app/data/poblacion.csv')
 @st.cache(suppress_st_warning=True)
 def cargarCrimenPob():
-    return pd.read_csv('./data/clean/crimen_pob.csv')
+    return pd.read_csv('./app/data/crimen_pob.csv')
